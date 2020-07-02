6c6f35d16e6a5a76088a77ab9ec4be1a53dd141f
Joshua Moses <62614287+joshuamoses-cyberark@users.noreply.github.com>
2020-07-02 13:54:56 -0500

    Update 1-install-master.sh

--- 1-install-master.sh
+++ 1-install-master.sh
@@ -3,9 +3,9 @@
 # INSTALL: Basic Script to install DAP Master Server (dap1)
 
 # Global Variables
-masterContainer="dap1"
-masterIP="10.0.0.100"
-version="11.2.1"		## Change to installation version
+masterContainer="conjur-appliance"
+masterIP="10.10.10.13"
+version="11.4.0"		## Change to installation version
 
 # Create Docker Container
 echo "Creating DAP Master Server Container"
