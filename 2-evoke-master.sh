e2ce7e654371ba6d374aabce87d97efaad8d8ab6
Joshua Moses <62614287+joshuamoses-cyberark@users.noreply.github.com>
2020-07-02 13:57:30 -0500

    Update 2-evoke-master.sh

--- 2-evoke-master.sh
+++ 2-evoke-master.sh
@@ -3,14 +3,14 @@
 # CONFIGURE: Basic Script to configure DAP container as Master Server (dap1)
 
 # Global Variables
-masterContainer="dap1"
+masterContainer="conjur-appliance"
 serverType="master"
 masterDNS="dap1.cyber-ark-demo.local"
 clusterDNS="dap-master.cyber-ark-demo.local"
 standby1DNS="dap2.cyber-ark-demo.local"
 standby2DNS="dap3.cyber-ark-demo.local"
-adminPass="Cyberark1"
-accountName="CAU"
+adminPass="MyCyber@rk01"
+accountName="myorg"
 
 # EVOKE: Execute evoke command to configure DAP container as Master Server
 docker exec $masterContainer evoke configure $serverType --accept-eula -h $masterDNS --master-altnames "$clusterDNS,$standby1DNS,$standby2DNS" -p $adminPass $accountName
